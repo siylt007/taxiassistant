b9722a4f2c64659159a823fa0ab265deccda19a2
shunbaojiang <shunbaojiang@pateo.com.cn>
2018-09-04 17:48:20 +0800

    提交整体代码
    Change-Id: If19e34c48c8028a67d0165261f120cbf97586842

---
+++ taxiassistant/src/main/java/com/taxiassistant/bottomdialog/ShareBottomDialog.java
@@ -0,0 +1,22 @@
+package com.taxiassistant.bottomdialog;
+
+import android.view.View;
+
+import com.taxiassistant.R;
+
+/**
+ * Created by shaohui on 16/10/11.
+ */
+
+public class ShareBottomDialog extends BaseBottomDialog{
+
+    @Override
+    public int getLayoutRes() {
+        return R.layout.dialog_layout;
+    }
+
+    @Override
+    public void bindView(View v) {
+        // do any thing you want
+    }
+}
